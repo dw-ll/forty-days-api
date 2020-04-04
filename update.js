3c8670a8e975ec3cf18a9cecc568c9a3489f059a
Daniel Williams <danswilliams95@gmail.com>
2020-04-04 16:22:02 -0700

    Added an update note endpoint.

---
+++ update.js
@@ -0,0 +1,28 @@
+import * as dynamoDbLib from './libs/dynamodb-lib.js';
+import { success, failure } from './libs/response-lib.js';
+
+export async function main(event, context) {
+    const data = JSON.parse(event.body);
+    const params = {
+        TableName: process.env.tableName,
+        // 'UpdateExpression': The attributes that are being set.
+        // 'ExpressionAttributeValues': Sets the attruibutes in the update expression.
+        Key: {
+            userId: event.requestContext.identity,
+            noteId: event.pathParameters.id
+        },
+        UpdateExpression: 'set content = :content, attachment = :attachment',
+        ExpressionAttributeValues: {
+            ':attachment': data.attachment || null,
+            ':content': data.content || null
+        }
+    };
+
+
+    try {
+        await dynamoDbLib.call('update', params);
+        return success({ status: true });
+    } catch (err) {
+        return failure({ status: false });
+    }
+}
