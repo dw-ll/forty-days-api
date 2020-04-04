5be002ccdec14a99d4e6f718707ccbe8c6e89493
Daniel Williams <danswilliams95@gmail.com>
2020-04-04 15:45:01 -0700

    Added a list notes endpoint.

---
+++ list.js
@@ -0,0 +1,27 @@
+import * as dynamoDbLib from './libs/dynamodb-lib.js';
+import { success, failure } from './libs/response-lib.js';
+
+export async function main(event, context) {
+    const params = {
+        TableName: process.env.tableName,
+        // 'KeyConditionExpression' : defines condition for query,
+        // 'userId' : only return items that match this, partition key,
+        // 'ExpressionAttributeValues' : defines the value in condition
+        // ':userId' : defines 'userId' to be Identitiy Pool identity id
+        // of the authenticated user
+
+        KeyConditionExpression: "userId = :userId",
+        ExpressionAttributeValues: {
+            ":userId": event.requestContext.identity.cognitoIdentityId
+        }
+    };
+
+    try {
+        const result = await dynamoDbLib.call('query', params);
+        // Query accesses items from a table by primary key or secondary index.
+        console.log(result);
+        return success(result.Items);
+    } catch (err) {
+        return failure({ status: false });
+    }
+}
